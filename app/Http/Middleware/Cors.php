7dec3ecfdd4e726cf35ed1a1142b9bf7706aee02
Laxus-Dreyarr <carljamesduallo661@gmail.com>
2025-12-19 23:08:49 +0800

    UPDATE THE cors GET only when using API

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -11,7 +11,8 @@ class Cors
         $response = $next($request);
         
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        // $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-API-Key');
         
         return $response;
