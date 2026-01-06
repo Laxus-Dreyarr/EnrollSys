b20555f68f816c26eb808ff4aa65c56d8205c487
Laxus-Dreyarr <carljamesduallo661@gmail.com>
2026-01-06 18:41:02 +0800

    Fixed the Logo - Student/Org

--- resources/views/student/dashboard/dashboard.blade.php
+++ resources/views/student/dashboard/dashboard.blade.php
@@ -42,7 +42,7 @@ if (isset($averageGrade) && $averageGrade > 0) {
     <meta name="msapplication-navbutton-color" content="#101126">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <title>enrollsys evsu</title>
-    <link rel="website icon" href="{{ asset('img/logo.png') }}">
+    <link rel="website icon" href="{{ asset('img/evsu-logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="{{ asset('style/google-fonts.css') }}" rel="stylesheet">
